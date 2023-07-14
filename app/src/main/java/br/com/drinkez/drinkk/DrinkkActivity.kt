c4ea6e48e19bee997ab37286fc828a3d1646d35f
Arthur Barbosa de Oliveira <arthurvl98@gmail.com>
2023-07-14 11:19:32 -0300

    terminando segunda page

---
+++ app/src/main/java/br/com/drinkez/drinkk/DrinkkActivity.kt
@@ -0,0 +1,69 @@
+package br.com.drinkez.drinkk
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.comfiap.primeiroaplicativo.databinding.ActivityBegginBinding
+import br.comfiap.primeiroaplicativo.databinding.ActivityDrinkkBinding
+
+class DrinkkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDrinkkBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        // Iniciando o view binding
+        binding = ActivityDrinkkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.caipirinha.setOnClickListener {
+            Toast.makeText(
+                this,
+                "caipirinhas",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.shots.setOnClickListener {
+            Toast.makeText(
+                this,
+                "shots",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.famous.setOnClickListener {
+            Toast.makeText(
+                this,
+                "populares",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.a.setOnClickListener {
+            Toast.makeText(
+                this,
+                "a",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.b.setOnClickListener {
+            Toast.makeText(
+                this,
+                "b",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+        binding.c.setOnClickListener {
+            Toast.makeText(
+                this,
+                "c",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+    }
+
+}
